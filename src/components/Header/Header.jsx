a03eafb5f9220dfd753f7ddc66970c3f119790ad
abhiTamta <abhinay.tamta@hotmail.com>
2024-08-12 12:51:12 +0530

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { FaToolbox } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-screen bg-white h-12 px-5 shadow-sm flex justify-between items-center">
+      <div className="logo">
+        <Link className="flex gap-2" to={"/"}>
+          <span><FaToolbox className="text-amber-500" size={25} /></span>
+          <span className="text-xl font-bold">Pixels<span className="text-red-500">JOBS</span></span></Link>
+      </div>
+      <div className="menu">menu</div>
+      <div className="search">search</div>
+      <div className="button">buttom</div>
+    </header>
   )
 }
 
