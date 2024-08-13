8be8350f47ac58a09d1135ef5a012516d9bc6896
abhiTamta <abhinay.tamta@hotmail.com>
2024-08-13 10:57:21 +0530

    login page commit

--- src/pages/employes/employes_login.jsx
+++ src/pages/employes/employes_login.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const EmployesLogin = () => {
   return (
     <div className="py-10 px-5 relative">
-      <div className="container mx-auto flex justify-center items-center flex-col md:flex-row">
-        <div className="w-4/12 rounded-md overflow-auto bg-white shadow-md p-5">
+      <div className="container mx-auto flex justify-center items-center flex-col gap-10 md:flex-row md:gap-0">
+        <div className="w-full order-2 rounded-md overflow-auto bg-white shadow-md p-5 md:order-1 lg:w-4/12">
           <div className="images">
             <img src="./../images/login_clipart.png" alt="" />
           </div>
@@ -12,15 +13,15 @@ const EmployesLogin = () => {
             <button type="button" className="rounded-md border border-solid border-blue-700 text-blue-700 py-3 px-5 hover:bg-blue-500 hover:text-white">Register for Free</button>
           </div>
         </div>
-        <div className="loginForm w-4/12 p-16 bg-white shadow-lg rounded-md">
+        <div className="loginForm w-full order-1 p-5 bg-white shadow-lg rounded-md md:order-2 lg:w-4/12 lg:p-16 ">
           <h2 className="text-2xl font-bold mb-5">Login</h2>
           <form method="post">
             <div className="w-full flex flex-col mb-3">
-              <label htmlFor="username">Email ID / Username</label>
+              <label className="text-sm" htmlFor="username">Email ID / Username</label>
               <input className="border w-full border-solid border-blue-700 py-2 px-5 rounded-md" type="text" name="username" id="username" />
             </div>
             <div className="w-full flex flex-col mb-3">
-              <label htmlFor="password">Password</label>
+              <label className="text-sm" htmlFor="password">Password</label>
               <input className="border w-full border-solid border-blue-700 py-2 px-5 rounded-md" type="password" name="password" id="password" />
             </div>
             <div className="w-full text-right text-xs text-blue-600 mb-3">
@@ -32,10 +33,10 @@ const EmployesLogin = () => {
             <div className="loginOtpBtn text-center">
               <button className="text-sm text-blue-600" type="button">Use OTP to Login</button>
             </div>
-            <div className="w-full text-center py-3">
+            <div className="w-full text-center py-8">
               <hr />
             </div>
-            <div className="w-full">Sign in with Google</div>
+            <div className="w-full flex items-center justify-center gap-2"><FcGoogle size={30} />Sign in with Google</div>
           </form>
         </div>
       </div>
--- src/routers/EmployeLayout.jsx
+++ src/routers/EmployeLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Logo from '../components/Header/Logo'
+import Footer from "../components/Footer/Footer"
 
 const EmployeLayout = () => {
     return (
         <>
-            <header className='bg-white py-4 px-5 h-16 shadow-md flex items-center'>
+            <header className='bg-white py-4 px-5 h-16 shadow-md flex items-center sticky'>
                 <div className="container mx-auto">
                     <Logo /></div>
             </header>
             <Outlet />
+            <Footer />
         </>
     )
 }
