d983fa9ddc2a0c088456ec52a6777eb08fc47a22
abhiTamta <abhinay.tamta@hotmail.com>
2024-08-13 23:36:06 +0530

    commit

--- src/pages/employes/employes_registors.jsx
+++ src/pages/employes/employes_registors.jsx
@@ -4,24 +4,32 @@ const EmployesRegistors = () => {
   return (
     <div className={registorStyle.userRegistorSection}>
       <div className={`${registorStyle.wapper} lg:w-8/12`}>
-        <div className="title">
+        <div className={registorStyle.title}>
           <h1>Create your Naukri profile</h1>
           <h5>Search & apply to jobs from India's No.1 Job Site</h5>
         </div>
         <form method="post">
-          <div className="">
-            <label htmlFor="fullname">Fullname</label>
-            <input type="text" name="fullname" id="fullname" />
+          <div className={registorStyle.formControls}>
+            <label htmlFor="fullname">Full Name</label>
+            <input type="text" name="fullname" id="fullname" placeholder="What is your name?" />
           </div>
 
-          <div className="">
+          <div className={registorStyle.formControls}>
             <label htmlFor="fullname">Email ID</label>
-            <input type="text" name="fullname" id="fullname" />
+            <input type="email" name="fullname" id="fullname" placeholder="Tell us your Email ID" />
+            <p>We'll send relevant jobs and updates to this email</p>
           </div>
 
-          <div className="">
+          <div className={registorStyle.formControls}>
             <label htmlFor="fullname">Password</label>
-            <input type="text" name="fullname" id="fullname" />
+            <input type="password" name="fullname" id="fullname" placeholder="(Minimum 8 characters)" />
+            <p>This helps your account stay protected</p>
+          </div>
+
+          <div className={registorStyle.formControls}>
+            <label htmlFor="fullname">Mobile Number</label>
+            <input type="tel" name="fullname" id="fullname" placeholder="Enter your mobile number" />
+            <p>Recruiters will contact you on this number</p>
           </div>
         </form>
       </div>
