4a454c176bc5afdad61942256fc0ea3e4162ed0c
abhiTamta <abhinay.tamta@hotmail.com>
2024-08-17 14:13:26 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { FaSearch } from 'react-icons/fa'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section>
+      <div className="container mx-auto flex justify-center flex-col">
+        <div className="headingTitle text-center pt-24 pb-16">
+          <h1 className='text-5xl font-bold mb-3'>Find your dream job now</h1>
+          <h4 className='text-xl'>5 lakh+ jobs for you to explore</h4>
+        </div>
+        <div className="jobSearchSection w-10/12 mx-auto p-4 rounded-3xl bg-white shadow-lg">
+          <form action="" method='post'>
+            <FaSearch />
+            <input type='text' name='' placeholder='Enter skill / ' id='' />
+            <select name="" id="">
+              <option value="1">1</option>
+            </select>
+            <input type="text" name='' placeholder='' id='' />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/employes/employes_registors.jsx
+++ src/pages/employes/employes_registors.jsx
@@ -1,4 +1,9 @@
+import { Flex, Radio } from 'antd';
+import experiencedIcon from "/images/employee_job_seeker.svg"
+import fresherIcon from "/images/employee_graduate_icon.svg"
 import registorStyle from "../../assets/css/Registor.module.css"
+
+
 
 const EmployesRegistors = () => {
   return (
@@ -10,26 +15,57 @@ const EmployesRegistors = () => {
         </div>
         <form method="post">
           <div className={registorStyle.formControls}>
-            <label htmlFor="fullname">Full Name</label>
+            <label htmlFor="fullname">Full Name<sup className="text-red-500">*</sup></label>
             <input type="text" name="fullname" id="fullname" placeholder="What is your name?" />
           </div>
 
           <div className={registorStyle.formControls}>
-            <label htmlFor="fullname">Email ID</label>
-            <input type="email" name="fullname" id="fullname" placeholder="Tell us your Email ID" />
-            <p>We'll send relevant jobs and updates to this email</p>
+            <label htmlFor="email">Email ID<sup className="text-red-500">*</sup></label>
+            <input type="email" name="email" id="email" placeholder="Tell us your Email ID" />
+            <p className="text-sm mt-1 text-gray-400">We'll send relevant jobs and updates to this email</p>
           </div>
 
           <div className={registorStyle.formControls}>
-            <label htmlFor="fullname">Password</label>
-            <input type="password" name="fullname" id="fullname" placeholder="(Minimum 8 characters)" />
-            <p>This helps your account stay protected</p>
+            <label htmlFor="password">Password<sup className="text-red-500">*</sup></label>
+            <input type="password" name="password" id="password" placeholder="(Minimum 8 characters)" />
+            <p className="text-sm mt-1 text-gray-400">This helps your account stay protected</p>
           </div>
 
           <div className={registorStyle.formControls}>
-            <label htmlFor="fullname">Mobile Number</label>
-            <input type="tel" name="fullname" id="fullname" placeholder="Enter your mobile number" />
-            <p>Recruiters will contact you on this number</p>
+            <label htmlFor="mobile">Mobile Number<sup className="text-red-500">*</sup></label>
+            <input type="tel" name="mobile" id="mobile" placeholder="Enter your mobile number" />
+            <p className="text-sm mt-1 text-gray-400">Recruiters will contact you on this number</p>
+          </div>
+
+          <div className={registorStyle.formControls}>
+            <label htmlFor="WorkStatus">Work status<sup className="text-red-500">*</sup></label>
+
+            <Flex vertical gap="middle">
+              <Radio.Group defaultValue="" className='flex gap-5'>
+                <Radio.Button value="a" className='h-auto rounded-none leading-none font-normal p-0 m-0 border-none bg-gray-300 border-gray-300'>
+                  <div className="flex items-center justify-between">
+                    <div className="">
+                      <h5>I'm experienced</h5>
+                      <p>I have work experience (excluding internships)</p>
+                    </div>
+                    <div className="w-14 p-2">
+                      <img src={experiencedIcon} alt="" />
+                    </div>
+                  </div>
+                </Radio.Button>
+                <Radio.Button value="b" className='h-auto rounded-none leading-none font-normal p-0 m-0 border-none bg-gray-300 border-gray-300'>
+                  <div className="flex items-center justify-between">
+                    <div className="">
+                      <h5>I'm a fresher</h5>
+                      <p>I am a student/ Haven't worked after graduation</p>
+                    </div>
+                    <div className="w-14 p-2">
+                      <img src={fresherIcon} alt="" />
+                    </div>
+                  </div>
+                </Radio.Button>
+              </Radio.Group>
+            </Flex>
           </div>
         </form>
       </div>
