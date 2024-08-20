7f159b6751bb8bd9d766d6c568a42b72fb120652
abhiTamta <abhinay.tamta@hotmail.com>
2024-08-20 11:11:08 +0530

    commit

--- src/pages/employes/employes_registors.jsx
+++ src/pages/employes/employes_registors.jsx
@@ -1,7 +1,131 @@
+import { Button, Checkbox, Form, Input, InputNumber, Select } from "antd"
+
 const EmployesRegistors = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
   return (
     <div className='container mx-auto py-10'>
-      <div className="w-10/12"></div>
+      <div className="w-2/3 mx-auto bg-white shadow-lg rounded-lg p-10">
+        <div className="mb-5">
+          <h1 className="font-bold text-[24px]">Create your job profile</h1>
+          <h5 className="text-[14px]">Search & apply to jobs from India's No.1 Job Site</h5>
+        </div>
+
+        <div className="registrationForm">
+          <Form onFinish={onFinish} form={form} layout="vertical" name="register">
+            <Form.Item
+              name="fullname"
+              label="Full Name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your full name!',
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+              
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              name="confirm"
+              label="Confirm Password"
+              dependencies={['password']}
+              
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('The new password that you entered do not match!'));
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+
+
+            <Form.Item
+              name="phone"
+              label="Phone Number"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your phone number!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item>
+              <Select>
+                <Select.Option value="male">Male</Select.Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              name="agreement"
+              valuePropName="checked"
+              rules={[
+                {
+                  validator: (_, value) =>
+                    value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+                },
+              ]}
+            >
+              <Checkbox>
+                I have read the <a href="">agreement</a>
+              </Checkbox>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   )
 }
