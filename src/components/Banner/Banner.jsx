bbc286ec05ec7aec5b1bc84dea743ca859788867
abhiTamta <abhinay.tamta@hotmail.com>
2024-09-13 00:11:45 +0530

    commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { Button, Flex, Form, Input, Select } from 'antd'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className='w-full mx-auto px-5 lg:w-2/3'>
+        <div className="flex justify-center flex-col">
+          <div className="headingTitle text-center py-24 px-4 pb-16">
+            <h1 className='text-5xl font-bold mb-3'>Find your dream job now</h1>
+            <h4 className='text-xl'>5 lakh+ jobs for you to explore</h4>
+          </div>
+
+          <div className="jobSearchSection p-5 bg-white mb-10">
+            <Form method='post'>
+              <Flex className='flex-col md:flex-row' align='center' justify='space-between'>
+                <Form.Item name={"searchSkill"} className='mb-0 w-full'>
+                  <Input className='w-full' size='large' type="text" placeholder='Enter skills / designations / companies' />
+                </Form.Item>
+                
+                <Form.Item name={"searchexperience"} className='mb-0 w-full'>
+                  <Select placeholder="Select experience" className='border-none'>
+                    <Select.Option>asfas</Select.Option>
+                    <Select.Option>asfas</Select.Option>
+                    <Select.Option>asfas</Select.Option>
+                  </Select>
+                </Form.Item>
+
+                <Form.Item name={"searchLocation"} className='mb-0 w-full'>
+                  <Input type='text' placeholder='Enter Location' />
+                </Form.Item>
+
+                <Form.Item className='mb-0 w-full'>
+                  <Button>Search</Button>
+                </Form.Item>
+              </Flex>
+            </Form>
+          </div>
+        </div>
+      </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,12 @@
-import { Button, Flex, Form, Input, Select } from 'antd'
-import React from 'react'
 import { BiHome } from 'react-icons/bi'
 import { FaSearch } from 'react-icons/fa'
+import Banner from '../components/Banner/Banner'
+import { Button } from 'antd'
 
 const Home = () => {
   return (
     <>
-      <section>
-        <div className="container mx-auto flex justify-center flex-col">
-          <div className="headingTitle text-center py-24 px-4 pb-16">
-            <h1 className='text-5xl font-bold mb-3'>Find your dream job now</h1>
-            <h4 className='text-xl'>5 lakh+ jobs for you to explore</h4>
-          </div>
-          <div className="jobSearchSection p-5 bg-white mb-10">
-            <Form method='post'>
-              <Flex className='flex-col md:flex-row' align='center' justify='space-between'>
-                <Form.Item name={"searchSkill"} className='mb-0'>
-                  <Input className='w-full' type="text" placeholder='Enter skills / designations / companies' />
-                </Form.Item>
-                
-                <Form.Item name={"searchexperience"} className='mb-0 '>
-                  <Select placeholder="Select experience" className='border-none'>
-                    <Select.Option>asfas</Select.Option>
-                    <Select.Option>asfas</Select.Option>
-                    <Select.Option>asfas</Select.Option>
-                  </Select>
-                </Form.Item>
-
-                <Form.Item name={"searchLocation"} className='mb-0'>
-                  <Input type='text' placeholder='Enter Location' />
-                </Form.Item>
-
-                <Form.Item className='mb-0'>
-                  <Button>Search</Button>
-                </Form.Item>
-              </Flex>
-            </Form>
-          </div>
-        </div>
-      </section>
+      <Banner />
       <section className='container mx-auto py-16'>
         <div className="flex flex-wrap gap-5 justify-center">
           <Button icon={<BiHome />} size='large'>Remote</Button>
