d848ca84afa9d00a0e605e6e878b951ef5e19d99
abhiTamta <abhinay.tamta@hotmail.com>
2024-09-13 13:10:40 +0530

    commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,41 +1,42 @@
 import { Button, Flex, Form, Input, Select } from 'antd'
-
+import "./search-section.css"
+import { CiSearch } from 'react-icons/ci'
 const Banner = () => {
   return (
-    <section className='w-full mx-auto px-5 lg:w-2/3'>
-        <div className="flex justify-center flex-col">
-          <div className="headingTitle text-center py-24 px-4 pb-16">
-            <h1 className='text-5xl font-bold mb-3'>Find your dream job now</h1>
-            <h4 className='text-xl'>5 lakh+ jobs for you to explore</h4>
-          </div>
-
-          <div className="jobSearchSection p-5 bg-white mb-10">
-            <Form method='post'>
-              <Flex className='flex-col md:flex-row' align='center' justify='space-between'>
-                <Form.Item name={"searchSkill"} className='mb-0 w-full'>
-                  <Input className='w-full' size='large' type="text" placeholder='Enter skills / designations / companies' />
-                </Form.Item>
-                
-                <Form.Item name={"searchexperience"} className='mb-0 w-full'>
-                  <Select placeholder="Select experience" className='border-none'>
-                    <Select.Option>asfas</Select.Option>
-                    <Select.Option>asfas</Select.Option>
-                    <Select.Option>asfas</Select.Option>
-                  </Select>
-                </Form.Item>
-
-                <Form.Item name={"searchLocation"} className='mb-0 w-full'>
-                  <Input type='text' placeholder='Enter Location' />
-                </Form.Item>
-
-                <Form.Item className='mb-0 w-full'>
-                  <Button>Search</Button>
-                </Form.Item>
-              </Flex>
-            </Form>
-          </div>
+    <section className='w-full mx-auto px-5 py-16 lg:w-3/4'>
+      <div className="flex justify-center flex-col">
+        <div className="headingTitle text-center py-12 px-4 pb-16">
+          <h1 className='text-5xl font-bold mb-3'>Find your dream job now</h1>
+          <h4 className='text-xl'>5 lakh+ jobs for you to explore</h4>
         </div>
-      </section>
+
+        <div className="jobSearchSection shadow-sm">
+          <Form method='post'>
+            <Flex justify='space-between' align='center' wrap="wrap" gap={20}>
+              <Form.Item name={"searchSkill"} className='m-0 flex-grow'>
+                <Input type="text" className='p-3' placeholder='Enter skills / designations / companies' />
+              </Form.Item>
+
+              <Form.Item name={"searchexperience"} className='m-0 flex-grow'>
+                <Select placeholder="Select experience" className='h-12'>
+                  <Select.Option>asfas</Select.Option>
+                  <Select.Option>asfas</Select.Option>
+                  <Select.Option>asfas</Select.Option>
+                </Select>
+              </Form.Item>
+
+              <Form.Item name={"searchLocation"} className='m-0 flex-grow'>
+                <Input type='text' className='p-3' placeholder='Enter Location' />
+              </Form.Item>
+
+              <Form.Item className='mb-0'>
+                <Button icon={<CiSearch />} className='searchButton'>Search</Button>
+              </Form.Item>
+            </Flex>
+          </Form>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,21 @@ const Home = () => {
   return (
     <>
       <Banner />
+
+      <section className='py-12'>
+        <div className="container mx-auto">
+          <div className="w-full text-center">
+            <h2 className='text-3xl mb-2'>Jobs you may be interested in</h2>
+            <p className='text-lg'>
+              Explore roles in tech, design, marketing, or finance—find your perfect fit!
+            </p>
+          </div>
+
+          <div className=""></div>
+        </div>
+      </section>
+
+
       <section className='container mx-auto py-16'>
         <div className="flex flex-wrap gap-5 justify-center">
           <Button icon={<BiHome />} size='large'>Remote</Button>
@@ -29,6 +44,7 @@ const Home = () => {
           <Button icon={<BiHome />} size='large'>Remote</Button>
         </div>
       </section>
+
       <section className='container mx-auto py-16'>
         <h2>Top companies hiring now</h2>
       </section>
