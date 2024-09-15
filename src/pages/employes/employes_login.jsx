da2c73ef8372a786f6444fb14db1c1128651d777
abhiTamta <abhinay.tamta@hotmail.com>
2024-09-15 21:42:57 +0530

    commit

--- src/pages/employes/employes_login.jsx
+++ src/pages/employes/employes_login.jsx
@@ -15,7 +15,7 @@ const EmployesLogin = () => {
 
   return (
     <div className="container mx-auto px- py-16 md:px-0 lg:px-0">
-      <div className="bg-white mx-auto w-full lg:w-8/12 flex justify-between">
+      <div className="bg-white mx-auto w-full flex justify-between lg:w-10/12 xl:w-8/12 2xl:w-6/12">        
         
         <div className="w-full hidden md:flex">
           <div className="images">
@@ -23,7 +23,7 @@ const EmployesLogin = () => {
           </div>
         </div>
 
-        <div className="w-full md:w-8/12 lg:w-10/12 xl:w-7/12 bg-blue-600 p-6">
+        <div className="w-full bg-blue-600 p-6 md:w-8/12 2xl:w-8/12">
           <div className="flex text-white gap-2 items-center mb-4">
             <TbUserHexagon size={40} />
             <h2 className="text-2xl font-bold"> Login</h2>
