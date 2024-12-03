b6352d131e6379c3cdca9cb569b90133be0b33c0
abhiTamta <abhinay.tamta@hotmail.com>
2024-12-03 12:52:31 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,23 @@ const Home = () => {
     <>
       <Banner />
 
+      <section className='container mx-auto py-16'>
+        <div className="w-full lg:w-9/12 mx-auto flex flex-wrap gap-5 justify-center">
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Remote</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>MNC</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Data Science</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Supply Chain</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Executive</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Sales</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Analytics</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>fortune 500</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Marketing</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>HR</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Fresher</Button>
+          <Button className='h-auto text-[18px] text-slate-500 py-3 px-5 hover:shadow-xl' icon={<BiHome />}>Project Manager</Button>
+        </div>
+      </section>
+
       {/* <section className='py-12'>
         <div className="container mx-auto">
           <div className="w-full text-center">
@@ -22,30 +39,7 @@ const Home = () => {
             <JobListingCard />
           </div>
         </div>
-      </section> */}
-
-      {/* <section className='container mx-auto py-16'>
-        <div className="flex flex-wrap gap-5 justify-center">
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-          <Button icon={<BiHome />} size='large'>Remote</Button>
-        </div>
-      </section> */}
+      </section> */}     
 
       {/* <section className='py-12'>
         <div className="container mx-auto">
@@ -60,13 +54,13 @@ const Home = () => {
             <TopCompanyCard />
           </div>
         </div>
-      </section> */}
+      </section>
       
-      {/* <section className='container mx-auto py-16'>
+      <section className='container mx-auto py-16'>
         <h2>Featured companies actively hiring</h2>
-      </section> */}
+      </section>
 
-      {/* <section className='container mx-auto py-16'>
+      <section className='container mx-auto py-16'>
         <h2>Sponsored companies</h2>
       </section> */}
     </>
